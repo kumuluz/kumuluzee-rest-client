dc9ebe003782da20bb1e7f2de54c94b89e0fa977
Klemen Kobau <klemen.kobau@gmail.com>
2019-03-29 10:26:06 +0100

    license, author, filter operation check

--- src/main/java/com/kumuluz/ee/rest/client/mp/integrations/KeeRestParameters.java
+++ src/main/java/com/kumuluz/ee/rest/client/mp/integrations/KeeRestParameters.java
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (c) 2014-2017 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.kumuluz.ee.rest.client.mp.integrations;
 
 import com.kumuluz.ee.rest.enums.FilterOperation;
@@ -13,6 +33,9 @@ import java.util.logging.Logger;
 
 /**
  * Used as bean parameter in rest client requests
+ *
+ * @author Klemen Kobau
+ * @since 1.3.0
  */
 public class KeeRestParameters {
 
@@ -69,6 +92,9 @@ public class KeeRestParameters {
         }
 
         public KeeRestParametersBuilder addFilter(String field, FilterOperation filterOperation, String value) {
+            if (filterOperation.equals(FilterOperation.ISNULL) || filterOperation.equals(FilterOperation.ISNOTNULL)) {
+                throw new IllegalArgumentException(filterOperation.toString() + " doesn't take the value parameter");
+            }
             filter.add(field + ":" + filterOperation.toString() + ":" + value);
             return this;
         }
